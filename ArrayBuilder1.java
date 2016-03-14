bff32483b745332c0d90232ced1a277207ea2f44
realbabyhippo <jk3466@columbia.edu>
2016-03-14 16:17:36 -0400

    Update and rename Problem1.java to ArrayBuilder1.java

--- ArrayBuilder1.java
+++ ArrayBuilder1.java
@@ -1,11 +1,11 @@
 //Juliette Kang
 // JK3466
-// Problem1.java - this class creates an array of rectangle objects and finds the largest rectangle based on its perimeter 
+// ArrayBuilder.java - this class creates an array of rectangle objects and finds the largest rectangle based on its perimeter 
 
 
 import java.util.Arrays;
 
-public class Problem1
+public class ArrayBuilder
 {
         public static void main(String args[])
         {
